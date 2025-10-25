01a9629eed5e927d5bae74947c380eebe2885cd6
Ashok-6 <ashokaksy123@gmail.com>
2025-10-26 00:22:37 +0530

    updating

--- src/api/api.js
+++ src/api/api.js
@@ -43,6 +43,8 @@ import axios from "axios";
 
 const API_BASE = import.meta.env.VITE_API_URL + "/documents";
 
+
+
 export const getDocuments = () => axios.get(API_BASE);
 
 export const uploadDocument = (formData) =>
