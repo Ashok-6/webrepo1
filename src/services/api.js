0b82fd3be5ea69dcb20fbc449d76646edb7897c0
Ashok-6 <ashokaksy123@gmail.com>
2025-10-25 20:48:32 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -8,16 +8,33 @@
 // export const uploadDocument = (formData) => axios.post(`${API_BASE}/upload`, formData);
 // export const deleteDocument = (id) => axios.delete(`${API_BASE}/${id}`);
 
+
+
+// import axios from "axios";
+
+// // For Vite:
+// const API_BASE = import.meta.env.VITE_API_URL + "/documents";
+
+// // For CRA, use: 
+// // const API_BASE = process.env.REACT_APP_API_BASE + "/documents";
+
+// export const getDocuments = () => axios.get(API_BASE);
+// export const uploadDocument = (formData) => axios.post(`${API_BASE}/upload`, formData);
+// export const deleteDocument = (id) => axios.delete(`${API_BASE}/${id}`);
+
+
 import axios from "axios";
 
-// For Vite:
 const API_BASE = import.meta.env.VITE_API_URL + "/documents";
 
-// For CRA, use: 
-// const API_BASE = process.env.REACT_APP_API_BASE + "/documents";
-
+// Get all documents
 export const getDocuments = () => axios.get(API_BASE);
-export const uploadDocument = (formData) => axios.post(`${API_BASE}/upload`, formData);
+
+// Upload a document
+export const uploadDocument = (formData) => 
+    axios.post(`${API_BASE}/upload`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+    });
+
+// Delete a document by ID
 export const deleteDocument = (id) => axios.delete(`${API_BASE}/${id}`);
-
-
