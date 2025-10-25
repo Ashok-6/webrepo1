840383f1ab54a05939862af362202847d408de0f
Ashok-6 <ashokaksy123@gmail.com>
2025-10-25 16:31:53 +0530

    bestone

--- src/services/api.js
+++ src/services/api.js
@@ -19,3 +19,5 @@ const API_BASE = import.meta.env.VITE_API_URL + "/documents";
 export const getDocuments = () => axios.get(API_BASE);
 export const uploadDocument = (formData) => axios.post(`${API_BASE}/upload`, formData);
 export const deleteDocument = (id) => axios.delete(`${API_BASE}/${id}`);
+
+
